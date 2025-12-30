08b3f9b81fd105e029b55dc401afcd972dd0bf26
Max Studi <1max.do@gmail.com>
2025-12-30 15:49:23 +0100

    Add Cookiebot consent script

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import './globals.css';
 
 export const metadata = {
@@ -8,6 +9,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <head>
+                <Script
+                    id="Cookiebot"
+                    src="https://consent.cookiebot.com/uc.js"
+                    data-cbid="49067d3a-76f2-4edf-8fe9-dfacf982f191"
+                    data-blockingmode="auto"
+                    strategy="beforeInteractive"
+                />
+            </head>
             <body>
                 <div className="bg-glow"></div>
                 {children}
